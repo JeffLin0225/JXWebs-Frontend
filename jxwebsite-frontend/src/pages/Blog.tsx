5f902c78a7622b59944a39585e63f91b68c3ab57
jeff <jefflin0225@gmail.com>
2025-02-09 18:08:59 +0800

    修正 img , code 顯示不出來問題 [ 穩定版 ]

--- jxwebsite-frontend/src/pages/Blog.tsx
+++ jxwebsite-frontend/src/pages/Blog.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import BlogNavbar from '../Blog/BlogNavbar';
 import '../Blog/Blog.css'; // 確保引入 CSS 檔案
-import sanitizeHtml from 'sanitize-html';
-
 
 const Blog: React.FC = () => {
   const [currentContent, setCurrentContent] = useState<string | null>(null);
@@ -18,37 +16,23 @@ const Blog: React.FC = () => {
     setCurrentUpdatetime( updatetime)
   };
 
-  const sanitizedContent = currentContent 
-    ? sanitizeHtml(currentContent, {
-        allowedTags: [
-          'p', 'br', 'b', 'i', 'strong', 'em', 
-          'u', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-          'ul', 'ol', 'li', 'span'
-        ],
-        allowedAttributes: {
-          // 如果需要保留某些屬性
-          'span': ['style']
-        }
-      }) 
-    : '';
-
   return (
     <div className="blog-container">
       <BlogNavbar onItemClick={handleItemClick} />
       <div className="blog-content">
         {currentContent ? (
           <>
-          <span style={{fontSize:'xx-large' , textDecoration:'underline' ,color :'wheat'}}>{currentSubject}</span>&emsp;	&emsp;
-          <span style={{ fontSize: '0.9rem', color: 'white', textDecoration : 'underline'}}>
-          {currentCreatetime ? `發佈時間：${new Date(currentCreatetime).toLocaleString()}` : '無日期'}
-          </span>&emsp;	&emsp;
-          <span style={{ fontSize: '0.9rem', color: 'white', textDecoration : 'underline'}}>
-          {currentUpdatetime ? `更新時間：${new Date(currentUpdatetime).toLocaleString()}` : ''}
-          </span>
-          <div 
-          dangerouslySetInnerHTML={{ __html: sanitizedContent }} 
-          className="blog-content-html" 
-          />
+            <span style={{ fontSize: 'xx-large', textDecoration: 'underline', color: 'wheat' }}>{currentSubject}</span>&emsp;&emsp;
+            <span style={{ fontSize: '0.9rem', color: 'white', textDecoration: 'underline' }}>
+              {currentCreatetime ? `發佈時間：${new Date(currentCreatetime).toLocaleString()}` : '無日期'}
+            </span>&emsp;&emsp;
+            <span style={{ fontSize: '0.9rem', color: 'white', textDecoration: 'underline' }}>
+              {currentUpdatetime ? `更新時間：${new Date(currentUpdatetime).toLocaleString()}` : ''}
+            </span>
+            <div 
+              dangerouslySetInnerHTML={{ __html: currentContent }} 
+              className="blog-content-html" 
+            />
           </>
         ) : (
           <p className="blog-empty-state">請選擇一篇有興趣的文章～</p>
@@ -56,6 +40,6 @@ const Blog: React.FC = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Blog;
